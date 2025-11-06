5f2f30bb6c07d2aa5f856f36247c3ab0cedd74ae
Melikov <melikvov@mail.ru>
2025-11-07 00:28:25 +0300

    Test added

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -5,12 +5,7 @@ const page = {
   anotherPage : 'Список литературы',
   block1: "Блок 1. Введение в экосистему JS-фреймворков. Основы React. ",
   block2: "Блок 2. Управление состоянием и продвинутые техники в React-приложениях. ",
-  finaltest: {
-    title: "Итоговое тестирование.",
-    type: "page",
-    href: 'https://wingkwong.github.io/react-quiz-component/',
-    newWindow: true,
-  }
+  finaltest: "Итоговое тестирование"
 }
 
 export default page
---
+++ src/components/test-locale.ts
@@ -0,0 +1,11 @@
+export const appLocale = {
+  "landingHeaderText": "<questionLength> вопросов",
+  "question": "Вопрос",
+  "startQuizBtn": "Начать тест!",
+  "resultFilterAll": "Все результаты",
+  "resultFilterCorrect": "Верные",
+  "resultFilterIncorrect": "Нерные",
+  "prevQuestionBtn": "Пред.",
+  "nextQuestionBtn": "След.",
+  "resultPageHeaderText": "Вы прошли тестирование. Вы правильно ответили на <correctIndexLength> из <questionLength> questions."
+} 
